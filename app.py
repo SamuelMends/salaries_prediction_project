6767e41b7ccf77660671211aba1d2ed360cf8813
SamuelMends <samuelqmendes@gmail.com>
2024-07-25 18:33:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ from sklearn.preprocessing import StandardScaler
 
 # Cria o app
 app = Flask(__name__)
+
+# Carregando o modelo e o padronizador:
+modelo_carregado = pickle.load(open('salary.model.pkl', 'rb'))
+modelo_sam = modelo_carregado["model"]
+country_mapping = modelo_carregado["country_mapping"]
+education_mapping = modelo_carregado["education_mapping"]
+devtype_mapping = modelo_carregado["devtype_mapping"]
+scaler_sam = pickle.load(open('dsam_scaler.pkl', 'rb'))
